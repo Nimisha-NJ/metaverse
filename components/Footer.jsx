3116c32f8621180668bc6fdf53e2ab549231f577
Ajith Joseph C <ajith.joseph@altd.in>
2023-11-02 17:23:18 +0530

    Nov 2,2023

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,26 @@
 'use client';
 
+import {  motion} from "framer-motion";
+import { socials } from "../constants";
+import styles from "../styles";
+import { footerVariants} from "../utils/motion";
+
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="flex flex-col">
+      <div className="mb-[50px] h-[2px] bg-white opacity-10">
+        <div className="flex items-center justify-between flex-wrap gap-4">
+        
+         
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { motion } from "framer-motion";
-import { TypingText, ExploreCard, TitleText } from "../components";
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { exploreWorlds } from "../constants";
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { TypingText, ExploreCard, TitleText } from '../components';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '../constants';
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2');
   return (
     <section className={`${styles.paddings}`} id="explore">
       <motion.div
@@ -21,10 +21,7 @@ const Explore = () => {
         <TypingText title="| The World" textStyles="text-center" />
         <TitleText
           title={
-            <>
-              Choose the world you want <br className="md:block hidden" /> to
-              explore
-            </>
+            <>Choose the world you want <br className="md:block hidden" /> to explore</>
           }
           textStyles="text-center"
         />
